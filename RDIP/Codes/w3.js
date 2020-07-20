6812ed8e0b2535405d572a65ad05a10207c1c998
Sumana <sumanap333@gmail.com>
2020-07-20 16:28:21 +0530

    Goal-8

--- RDIP/Codes/w3.js
+++ RDIP/Codes/w3.js
@@ -1,14 +1,12 @@
 $("#rf").hide();
 $("#cc").hide();
 $("#cfs").hide();
-var se1 = ["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple","some students like to study in the night","at night some students like to study","John and Mary went to church","Mary and John went to church","John went to church after eating","after eating John went to church","John after eating went to church","did he go to market","he did go to market","the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics","John goes to the library and studies","John studies and goes to the library","John ate an apple so did she","she ate an apple so did John","the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book","I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]
-var sh1 = ["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम","राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी","मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम","राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम","बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को","एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब","एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]
-var a = Math.floor(Math.random()*se1.length);
-var b =se1[a];
-var c = s(b);
-var a1 = Math.floor(Math.random()*sh1.length);
-var b1 = sh1[a1];
-var c1 = s(b1);
+$("#w").hide();
+var e1 = [["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],["some students like to study in the night","at night some students like to study"],["John and Mary went to church","Mary and John went to church"],["John went to church after eating","after eating John went to church","John after eating went to church"],["did he go to market","he did go to market"],["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"],["John goes to the library and studies","John studies and goes to the library"],["John ate an apple so did she","she ate an apple so did John"],["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]];
+var se = ["John ate an apple before afternoon","some students like to study in the night","John and Mary went to church","John went to church after eating","did he go to market","the woman who called my sister sells cosmetics","John goes to the library and studies","John ate an apple so did she","the teacher returned the book after she noticed the error","I told her that I bought a book yesterday"]
+var h1 = [["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"],["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"],["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"],["राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम"],["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"],["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]];
+var sh=["राम और श्याम बाजार गयें","राम सोया और श्याम भी","मैंने उसे बताया कि राम सो रहा है","राम खाकर सोया","बिल्लियों को मारकर कुत्ता सो गया"	,"एक लाल किताब वहाँ है","एक बड़ी सी किताब वहाँ है	"]
+
 function display(a){
 	if(a == "eng"){
 		document.getElementById("ds").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words"+"<br>"
@@ -28,10 +26,16 @@ function display(a){
 
 function es() {
 	// body...
+	var a = Math.floor(Math.random()*se.length);
+    var b =se[a];
+    var c = s(b);
 	button(c);
 }
 function hs() {
 	// body...
+	var a1 = Math.floor(Math.random()*sh.length);
+var b1 = sh[a1];
+var c1 = s(b1);
 	button(c1);
 }
 function s(n) {
@@ -67,25 +71,23 @@ function button(n) {
  	document.getElementById("rf").innerHTML = null;
  	document.getElementById("cfs").innerHTML = null;
  	var cnt1 =0;
+ 	var str = " "
  	$(".nc").click(function(){
  		var vv = $(this).text();
  		document.getElementById("cfs").innerHTML += vv + " ";
 			$(this).hide();
 			cnt1 = cnt1+1;
-			count();
-     		var v = $(this).text();
-			document.getElementById("ss1").innerHTML += v + " ";
+			document.getElementById("ss1").innerHTML += vv + " ";
 			document.getElementById("fs").innerHTML = "<br>"+"<b>Formed Sentence</b>"+"<i>(after selecting words)</i>"+"<br>";
 			$("#rf").show();
 			document.getElementById("rf").innerHTML = "Re-form the sentence"
-			function count(){
-				if(cnt == cnt1){
-					$("#cc").show();
-				}
-			}
+			str = str+vv+" "
+			count(str)
      })
      document.getElementById("ss1").innerHTML = " "
      document.getElementById("fs").innerHTML = " "
+     document.getElementById("r").innerHTML = " "
+     document.getElementById("w").innerHTML = " "
      $("#rf").hide();
      $("#cc").hide();
      $("#rf").click(function(){
@@ -98,22 +100,53 @@ function button(n) {
 		$("#ss1").show();
 		$("#fs").show();
 })
+     function count(n){
+				if(cnt == cnt1){
+					$("#cc").show();
+					var cn1 = 0;
+					$("#cc").click(function(){
+						var ind = document.getElementById("select").selectedIndex;
+						var val = document.getElementById("select")[ind].value;
+						var s1 = n.trim();	
+						if(val == "eng"){
+						for (var i = 0; i < 10; i++) {
+							for (var j = 0; j < e1[i].length; j++) {
+								if(s1 == e1[i][j]){
+									cn1++;
+								}
+							}
+						}
+						if(cn1 == 1){
+							$("#r").show()
+							$("#w").hide();
+							document.getElementById("r").innerHTML = "Right answer!!!"
+						}
+						else{
+							$("#w").show();
+							document.getElementById("w").innerHTML = "Wrong answer!!!"
+						}
+					}
+					else{
+						for (var i = 0; i < 7; i++) {
+							for (var j = 0; j < h1[i].length; j++) {
+								if(s1 == h1[i][j]){
+									cn1++;
+								}
+							}
+						}
+						if(cn1 == 1){
+							$("#r").show()
+							$("#w").hide();
+							document.getElementById("r").innerHTML = "Right answer!!!"
+						}
+						else{
+							$("#w").show();
+							document.getElementById("w").innerHTML = "Wrong answer!!!"
+						}
+					}
+					})
+				}
+			}
      }
      a();
 }
-
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
